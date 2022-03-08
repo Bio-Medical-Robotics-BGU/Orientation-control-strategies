283d5f1ecd20bdac40e799f79e3337d78c68bce6
orzruya <orzr@post.bgu.ac.il>
2022-03-08 11:24:59 +0200

    exp1 - until MK

---
+++ exp1/plotSphere.m
@@ -0,0 +1,15 @@
+function [] = plotSphere()
+%% Plot a full grey sphere
+hold on
+[x_s,y_s,z_s] = sphere(50);
+h = surf(x_s,y_s,z_s);
+h.EdgeColor = 'None';
+set(h, 'FaceAlpha', 0.4)
+hidden off
+axis equal
+axis off
+map = repmat((linspace(1,0,100))',1,3);
+colormap(map)
+h.EdgeColor = 'interp';
+end
+
